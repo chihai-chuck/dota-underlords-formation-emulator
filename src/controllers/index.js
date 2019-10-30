c2f95cdcf5c4cc65308c90c797df0ed487872bb4
Chuck <chuck@chihai.me>
2019-10-30 23:53:00 +0800

    还原代码.....

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -309,12 +309,13 @@ window.dotaUnderlordsFormationEmulator = new Vue({
             this.alert("保存的阵容链接已复制到剪贴板");
         },
         restoreFormationURL(data) {
+            const code = decodeURIComponent(data);
             let jsonStr = "";
             // 之前BUG导致分享链接被url编码两次，解码时无法正常atob，此处代码为兼容解析之前BUG的分享链接
             try {
-                jsonStr = window.atob(data);
+                jsonStr = window.atob(code);
             } catch {
-                jsonStr = window.atob(decodeURIComponent(data));
+                jsonStr = window.atob(decodeURIComponent(code));
             }
             for(let item of JSON.parse(jsonStr)) {
                 this.$set(this.data.chessboard, item.i, {
